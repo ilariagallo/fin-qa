e140abdfc8045af033f5e5576c92f2af1172e0f1
ilariagallo <ilar.gallo@gmail.com>
2024-12-05 13:35:55 +0000

    Add functionality to create report

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,27 +4,24 @@ from langchain_openai import ChatOpenAI
 
 from src.prompts import fin_qa_evaluation_prompt
 
-# Grade prompt
-grade_prompt_answer_accuracy = prompt = hub.pull("langchain-ai/rag-answer-vs-reference")
-grade_prompt_answer_accuracy.messages[0].prompt.template = fin_qa_evaluation_prompt
-llm = ChatOpenAI(model="gpt-4o-mini")
-
 
 class AnswerEvaluator:
+    """
+    A simple evaluator for RAG answer accuracy
+    """
 
     def __init__(self, custom_prompt=None):
         self.llm = ChatOpenAI(model="gpt-4o-mini")
         self.grader_prompt = hub.pull("langchain-ai/rag-answer-vs-reference")
         self.grader_prompt.messages[0].prompt.template = custom_prompt
-        self.answer_grader = grade_prompt_answer_accuracy | llm
+        self.answer_grader = self.grader_prompt | self.llm
 
     def evaluate_answer(self, qa: pd.Series) -> list:
         """
-        A simple evaluator for RAG answer accuracy
+        Runs evaluation on a single question
 
         :param qa: pd.Series including question, actual answer and expected answer for a single question
         :returns score and explanation
-
         """
 
         # Get question, ground truth answer, RAG chain answer
@@ -41,15 +38,48 @@ class AnswerEvaluator:
 
         return [score, explanation]
 
-    def run(self, input_file, output_file):
-        predictions = pd.read_csv(input_file)
+    def run(self, input_filepath, output_filepath):
+        """
+        Runs evaluation on all predictions in the input file
+
+        :param input_filepath: filepath to file with the predictions
+        :param output_filepath: filepath to file with scores and explanations
+        """
+        predictions = pd.read_csv(input_filepath)
         scores = predictions.apply(lambda row: self.evaluate_answer(row), axis=1)
         scores_df = pd.DataFrame(data=list(scores), columns=['score', 'explanation'])
 
         final_df = pd.concat([predictions, scores_df], axis=1)
-        final_df.to_csv(output_file, index=False)
+        final_df.to_csv(output_filepath, index=False)
+
+    @staticmethod
+    def create_report(evaluation_filepath, report_filepath):
+        """
+
+        :param evaluation_filepath: filepath to file with scores and explanations
+        :param report_filepath: filepath to report file
+        """
+        evaluation = pd.read_csv(evaluation_filepath)
+        scores = evaluation['score']
+
+        # Count occurrences of each value
+        counts = scores.value_counts().sort_index()
+
+        # Calculate percentages
+        percentages = (counts / len(scores)) * 100
+
+        # Combine counts and percentages into a DataFrame
+        result = pd.DataFrame({'Count': counts, 'Percentage (%)': percentages})
+
+        result.to_csv(report_filepath)
 
 
 if __name__ == "__main__":
+    data_folder = '../data/'
+    predictions_filepath = data_folder + 'results-solution-1.csv'
+    evaluation_filepath = data_folder + 'results-solution-1-evaluated.csv'
+    report_filepath = data_folder + 'results-solution-1-report.csv'
+
     evaluator = AnswerEvaluator(fin_qa_evaluation_prompt)
-    evaluator.run("../data/results-solution-1.csv", '../data/results-solution-1-evaluated.csv')
+    # evaluator.run("../data/results-solution-1.csv", '../data/results-solution-1-evaluated.csv')
+    evaluator.create_report(evaluation_filepath, report_filepath)
